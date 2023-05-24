a3c32d28a0dd305be85bc8f6fb32d752c1fb11a3
muhammednabeel123 <muhammednabeel237@gmail.com>
2023-05-24 07:11:20 +0530

    bootstrappin in angular completed

--- basics/src/app/app.module.ts
+++ basics/src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NgModule  } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports : [BrowserModule],
+  declarations :[AppComponent],
+  bootstrap:[AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+
+}

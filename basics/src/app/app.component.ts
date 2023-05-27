7fe875ca47ce6921264469283efc15f4d12e9933
muhammednabeel123 <muhammednabeel237@gmail.com>
2023-05-27 20:05:19 +0530

    event binding

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -10,7 +10,13 @@ export class AppComponent {
   name = 'Nabeel'
   imgUrl ='https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0'
 
+
   getName(){
     return this.name
+  }
+  changeImage(e:KeyboardEvent){
+    this.imgUrl = e.target.value
+     
+
   }
 }

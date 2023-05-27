fe41b637ccc1b2eaaaecd79af87f23dddb3fc053
muhammednabeel123 <muhammednabeel237@gmail.com>
2023-05-27 19:48:57 +0530

    property binding added

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'basics';
   name = 'Nabeel'
+  imgUrl ='https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0'
 
   getName(){
     return this.name
